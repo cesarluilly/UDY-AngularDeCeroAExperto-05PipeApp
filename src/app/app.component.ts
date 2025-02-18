95049082c1000726009ad462dd2f83a4d4cc9866
cesarluilly <cesarluilly.garcia@hotmail.com>
2025-02-18 08:25:44 -0600

    utilizando la version 17 de angular con caracteristicas de la version 15 de angular, como son los modulos ya que en la version 17 ya es obsoleto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { PrimeNGConfig } from 'primeng/api';
-import { SharedModule } from "./shared/shared.module";
-import { RouterOutlet } from '@angular/router';
+// import { SharedModule } from "./shared/shared.module";
+// import { RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
-  standalone: true,
+  // standalone: true,
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  imports: [SharedModule, RouterOutlet]
+  // imports: [SharedModule, RouterOutlet] --El import solo es valido si el component es standalone.
 })
 export class AppComponent implements OnInit{
 
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,21 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { AppComponent } from './app/app.component';
 
-bootstrapApplication(AppComponent, {
-  providers: [
-    provideRouter([])  // ✅ Agrega enrutamiento si lo usas
-  ]
-}).catch(err => console.error(err));
+
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
+
+//platformBrowserDynamic es para la version 15 de angular y tambien para la version 17
+//    si se quiere seguir trabajando con modulos.
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
+
+
+//bootstrapApplication solo es para angular17 ya que aqui en esta version manejan mucho los standalone,
+//  y al parecer es como si los modulos ya se volvieran obsoletos
+//Manera de usar cuando estamos utilizando StandaloneComponents
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideRouter([])  // ✅ Agrega enrutamiento si lo usas
+//   ]
+// }).catch(err => console.error(err));
